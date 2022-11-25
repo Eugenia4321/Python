8df8b4d64ccb051af84bb52a56517cf4d60726f8
Eugenia <evg_vit2015@gmail.com>
2022-11-25 17:37:42 +0500

    задача 4

---
+++ Семинар_1/Задача 4/Задача_4.py
@@ -0,0 +1,21 @@
+﻿#4-Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+#Пример:
+#- quarter = 1 => x∈(0, ∞) u y∈(0,∞)
+
+def Check_coordinates(quarter):
+    if quarter>4 or quarter< 1:
+        print('does not exist')
+    if quarter == 1:
+        print('x∈(0, ∞) u y∈(0, ∞)')
+    if quarter == 2:
+        print('x∈(∞, 0) u y∈(0, ∞)')
+    if quarter == 3:
+        print('x∈(∞, 0) u y∈(∞, 0)')
+    if quarter == 4:
+        print('x∈(0, ∞) u y∈(∞, 0)')
+
+
+
+Check_coordinates(int(input('quarter = ')))
+
+
