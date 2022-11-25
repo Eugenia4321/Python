657416b9dc0fd26cf01814f4d338fc42e3e38540
Eugenia <evg_vit2015@gmail.com>
2022-11-25 17:21:39 +0500

    задача2

--- Семинар_1/Задача1/Задача1.py
+++ Семинар_1/Задача1/Задача1.py
@@ -1,1 +1,20 @@
 
+#1- Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+#Дополнительно: можете проверить, что это действительно число, и что это действительно входит в нужный диапазон
+
+#Пример:
+
+#6 -> да
+#7 -> да
+#1 -> нет
+
+def Check_day_of_week(day_of_week):
+    if 1>day_of_week or day_of_week>7:
+        return 'wrong number'
+    elif day_of_week==6 or day_of_week==7:
+        return "yes"
+    else:
+        return 'no'
+      
+print(Check_day_of_week(int(input('Enter number = '))))
+
---
+++ Семинар_1/Задача2/Задача2.py
@@ -0,0 +1,13 @@
+﻿#2- Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+#Нужно написать таблицу истинности.
+
+
+# решение исходят из предположения что всё таки там нужны вторые скобки и задача выглядит ¬(X ⋁ Y ⋁ Z) = (¬X ⋀ ¬Y ⋀ ¬Z)
+def it_was_misstake_to_start_learn_programming():
+    for x in range(0, 2):
+        for y in range(0, 2):
+            for z in range(0,2):
+                print(f'x = {x}, y = {y}, z = {z} : ¬(X ⋁ Y ⋁ Z) = (¬X ⋀ ¬Y ⋀ ¬Z) is {not(x or y or z) == ((not x) and (not y) and (not z))}')
+
+
+it_was_misstake_to_start_learn_programming()
