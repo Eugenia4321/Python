2bc72e754ae4abb0d1300939b08b9b15b42c8d00
Eugenia <evg_vit2015@gmail.com>
2022-12-02 19:56:30 +0500

    задача 2

---
+++ Семинар_3/Задача_2_произведение_пар_чисел/Задача_2_произведение_пар_чисел.py
@@ -0,0 +1,16 @@
+
+
+
+
+numbers_list = [2, 3, 4,  5, 6]
+
+def product_numbers(numbers_list):
+    result_list = []
+    for i in range(0,int(len(numbers_list)/2)):
+        result_item =numbers_list[i]*numbers_list[len(numbers_list)-1-i]
+        result_list.append(result_item)
+    if (len(numbers_list))%2!=0:
+        result_list.append(numbers_list[int(len(numbers_list)/2)]**2)        
+    return result_list
+
+print(f'List: {numbers_list} \nResult list: {product_numbers(numbers_list)}')
