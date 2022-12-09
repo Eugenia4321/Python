f830adfa72e9a87b015da2147c9ef70d36f4cd73
Eugenia <evg_vit2015@gmail.com>
2022-12-09 23:25:02 +0500

    ХО

--- Семинар_5/Задача_2_игра_с_конфетами/Задача_2_игра_с_конфетами.py
+++ Семинар_5/Задача_2_игра_с_конфетами/Задача_2_игра_с_конфетами.py
@@ -41,11 +41,11 @@ def game_one_players(candy_count:int,step_count:int):
         if player_who =='Player':
             step_player = int(input(f'{player_who} takes candy: '))
         else:         
-            step_player = random.randint(1, min(step_count,candy_count))
-            if step_count<candy_count<=step_count*2:
-                step_player =candy_count-step_count-1
-            if candy_count<=step_count:
-                step_player =candy_count
+            step_player = random.randint(1, min(step_count,candy_count))   #обычный бот
+            if step_count<candy_count<=step_count*2:                       # умный бот 
+                step_player =candy_count-step_count-1                      # 
+            if candy_count<=step_count:                                    # 
+                step_player =candy_count                                   # 
         if candy_count-step_player<0 or step_player>step_count:
             print('You cant take so many!')
         else:
@@ -58,13 +58,20 @@ def game_one_players(candy_count:int,step_count:int):
             else:
                 player_who='Player'
 
-candy_count = int(input('Enter candy count = '))
 
+game_mode=int(input('Enter 1 for two players game mode and 2 for play with bot = '))
+
+candy_count = int(input('Enter candy count = '))
 step_count = int(input('Enter step count = '))
 
 while step_count>candy_count/3:
     print('Too large step!')
     step_count = int(input('Enter step count = '))
 
-#game_two_players(candy_count,step_count)
-game_one_players(candy_count,step_count)
+
+if game_mode==1:
+    game_two_players(candy_count,step_count)
+if game_mode==2:
+    game_one_players(candy_count,step_count)
+else:
+    print(f'No {game_mode} game mode')
--- Семинар_5/Задача_3_крестики_нолики/Задача_3_крестики_нолики.py
+++ Семинар_5/Задача_3_крестики_нолики/Задача_3_крестики_нолики.py
@@ -12,27 +12,6 @@ def print_game_map(game_map:list):
        
 print_game_map(game_map)
 
-#def game(game_map):
-#    player_number = 1
-#    win_chek=True 
-#    while win_chek:
-#        step = int(input(f'\nPlayer {player_number} do step : \n'))
-#        if game_map[step-1] == '0' or game_map[step-1] == 'X':
-#            print('Error\n')
-#        else:
-#            if player_number==1:
-#                game_map[step-1] = 'X'
-#                player_number=2
-               
-#            else:
-#                game_map[step-1] = '0'
-#                player_number=1
-#        print_game_map(game_map)
-#        win_chek=win_chek_function(game_map)
-#    if player_number == 1:
-#        print(f'Player 2 WIN')
-#    else:
-#        print(f'Player 1 WIN')
 def game(game_map:list):
     """ Игра в крестики нолики, принимает поле и возвращает номер проигравшего игрока или 0 если ничья
     Arguments:
@@ -41,10 +20,9 @@ def game(game_map:list):
              (int)player_number
     """
     player_number = 1
-   # win_chek=True 
-    for i in range(0,9): #while 
-        step = input_step(game_map,player_number)
- #       step = int(input(f'\nPlayer {player_number} do step : \n'))             
+  
+    for i in range(0,9): 
+        step = input_step(game_map,player_number)          
         if player_number==1:
             game_map[step-1] = 'X'
             player_number=2               
@@ -56,7 +34,14 @@ def game(game_map:list):
             return player_number
     return 0
 
-def input_step(game_map,player_number):    #проверкун на 0-9 потом проверку на х и 0
+def input_step(game_map,player_number):
+    """ Проверка на ввод шага - от 1 до 9 в незанятую ячейку, возвращает допустимый шаг
+    Arguments:
+             (list)game_map 
+             (int)player_number
+        Return:
+             (int)step
+    """    
     while True:
         step = int(input(f'\nPlayer {player_number} do step : \n'))
         if 0<step<9:
@@ -68,7 +53,7 @@ def input_step(game_map,player_number):    #проверкун на 0-9 пото
             print('error')
     
 def win_chek_function(game_map:list):
-    """�������� �� ���������� - ���������� False ���� ���� �� ������� �������
+    """Проверка на победителя, возвращает False если игрок победил и игра закончена
         
     """
     if game_map[0]==game_map[1]==game_map[2]:
