a778cab39891749a1430221a02971a36189a4f43
Eugenia4321 <113255505+Eugenia4321@users.noreply.github.com>
2022-12-26 20:11:54 +0500

    Update config.py

--- телеграмм/config.py
+++ телеграмм/config.py
@@ -1,3 +1,3 @@
-TOKEN = '5899734910:AAGbyD4Nx1U-UWlXemaXsWmoB8m6Nw_JXX8'
+TOKEN = ''
 ERROR_MESSAGE = 'Ой что то пошло не так!\nВведи /help что бы узнать, в чем ты ошибся'
 ERROR_MESSAGE_NOT_INT = 'Это не похоже на целые числа, попробуй еще'
